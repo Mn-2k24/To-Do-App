297e990978acf146eee0644f9118c30ca357c30e
Nizam ul din <164358510+Mn-2k24@users.noreply.github.com>
2024-04-20 15:48:10 +0500

    Add files via upload
    ToDo App is your ultimate companion for mastering your daily tasks and enhancing your productivity. Say goodbye to scattered sticky notes and forgotten tasks; with ToDo App, you can organize, prioritize, and conquer your to-do list with ease.

---
+++ main.ts
@@ -0,0 +1,127 @@
+#! /usr/bin/enu node
+import inquirer from "inquirer";
+
+//interfaces
+//array
+//function
+//operators
+
+interface TodoItem {
+
+    task: string;
+    completed: boolean;
+}
+//array
+
+let todolist: TodoItem[] = [];
+
+//function
+let mainMenu= async () =>{
+const { action } = await inquirer.prompt({
+
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: ["Add Task", "View List", "Mark as complete", "Delete Task", "Exit"]
+
+});
+
+
+
+switch ( action ) {
+    case "Add Task":
+           await addTask();
+           break;
+    case "View List":
+        viewList();
+        break;
+    case "Mark as complete":
+       await markCompleted();
+        break;
+    case "Delete Task":
+        await deleteTask();
+        break;
+    case "Exit":
+    case 'Exit':
+        console.log("good bye");
+        return;
+}
+mainMenu();
+}
+
+let addTask = async()=>{
+
+    let { task } = await inquirer.prompt({
+        type: "input",
+        name: "task",
+        message: "enter the task",
+    });
+    todolist.push({
+        task: task,
+        completed: false
+    });
+    console.log("task added successfully")
+};
+let viewList =  ()=> {
+console.log("*********To Do List**********");
+todolist.forEach((item, index) => {
+
+    console.log(`${index +1}.[${item.completed ? `x`:``}]${item.task}`)
+});
+    console.log("*************");
+};
+
+let markCompleted = async () => {
+    let{index} = await inquirer.prompt({
+
+     type: 'number',
+     name: 'index',
+     message: 'what task did you want to mark as completed?',
+    });
+    if(index<1 || index> todolist.length){
+        console.log('invalid task number');
+        return
+    }
+todolist[index -1].completed = true;
+console.log('task marked completed');
+
+ };
+ let deleteTask = async()=>{
+
+    let { index } = await inquirer.prompt({
+        type: "number",
+        name: "index",
+        message: "Select task to delete",
+    });
+    
+    if (index <=0 ){
+        console.log("Please Fist Add any task");
+        return;
+    }
+    
+    else if(index<1|| index> todolist.length){
+        console.log('invalid task number please try again');
+        return
+    };
+todolist.splice(index -1 , 1);
+    console.log("Task Deleted successfully")
+};
+ mainMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
